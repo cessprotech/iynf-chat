ea59dfb40b066542882afd565202ad7e058767e2
chucky-golden <chukschibyke92@gmail.com>
2024-09-25 18:17:43 +0100

    delete chat

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -199,6 +199,8 @@ export class AppService {
   }
 
   async deleteMessage(messageId: string, chatId: string, userId: string) {
+    console.log('deletedid', userId);
+    
     return await this.messageModel.findOneAndDelete({ messageId, chatId, authorId: userId });
   }
 
